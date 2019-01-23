61f9d1a5d375aeb201b301dc2e234bb5c2511411
Pedro Henrique <pedro.lg.cs@gmail.com>
2019-01-23 12:47:51 -0300

    add prefix in router

--- start/routes.js
+++ start/routes.js
@@ -2,23 +2,25 @@
 
 const Route = use('Route')
 
+// -------------------- Home -------------------- //
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
-})
+}).prefix('api/v1')
+// Router Group prefix
 
 // -------------------- Login -------------------- //
-Route.post('/sessions', 'SessionController.create')
+Route.post('/sessions', 'SessionController.create').prefix('api/v1')
 
 // -------------------- User -------------------- //
 Route.group(() => {
   Route.post('/user', 'UserController.store').as('user.store')
   Route.get('/user', 'UserController.show').as('user.show')
   Route.put('/user', 'UserController.update').as('users.update')
-}).middleware(['auth'])
+}).middleware(['auth']).prefix('api/v1')
 
-// -------------------- admin -------------------- //
+// -------------------- Admin -------------------- //
 Route.group(() => {
   // ----- Users -----
   Route.resource('/users', 'UserController').apiOnly()
     .middleware(['auth:jwt', 'is:(administrator)'])
-}).namespace('Admin')
+}).namespace('Admin').prefix('api/v1')
